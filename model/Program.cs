f2549c4383c9cd0a6e3e61c459e4ab92bde9a3d5
Niket <ac150381@avcol.school.nz>
2025-05-16 10:10:45 +1200

    made get all sports list

--- model/Program.cs
+++ model/Program.cs
@@ -5,12 +5,12 @@
         private static Storagemanager storagemanager;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
             string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial" +
                 " Catalog=\"sports managment \";Integrated Security=True;Connect " +
                 "Timeout=30;Encrypt=False;Trust Server Certificate=False;Application " +
                 "Intent=ReadWrite;Multi Subnet Failover=False";
-            Storagemanager hello = new Storagemanager(connectionString);
+            storagemanager  = new Storagemanager(connectionString);
             
         }
     }
--- model/StorageManager.cs
+++ model/StorageManager.cs
@@ -4,26 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
-namespace Sports_DB.model
+namespace Sports_DB.model;
+
+internal class Storagemanager
 {
-    internal class Storagemanager
+    private SqlConnection conn;
+
+    public Storagemanager(string connectionString)
     {
-        private SqlConnection conn;
-
-        public Storagemanager(string connectionString)
+        try
         {
-            try
-            {
-                conn = new SqlConnection(connectionString);
-                conn.Open();
-                Console.WriteLine("connection successful");
+            conn = new SqlConnection(connectionString);
+            conn.Open();
+            Console.WriteLine("your connection is successful");
 
-            }
-            catch (Exception E)
-            {
-                Console.WriteLine("connection NOT successful");
-                Console.WriteLine(E.Message);
-            }
+        }
+        catch (Exception E)
+        {
+            Console.WriteLine("your connection NOT successful");
+            Console.WriteLine(E.Message);
+        }
+    }
+
+    public List<sports> GetALLSports()
+    {
+        List<sports> sports = new List<sports>();
+        string sqlString = "SELECT * FROM tblSports";
+        using (SqlCommand cmd = new SqlCommand(sqlString, conn))
+        {
+            cmd.Parameters.Clear();
         }
 
 
