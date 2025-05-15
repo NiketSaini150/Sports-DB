820e8957eabb387effb2effa084b68fa03d1ec38
Niket <ac150381@avcol.school.nz>
2025-05-16 09:48:32 +1200

    added storage manager connection to database, and put in try, catch

---
+++ model/StorageManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+namespace Sports_DB.model
+{
+    internal class Storagemanager
+    {
+        private SqlConnection conn;
+
+        public Storagemanager(string connectionString)
+        {
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+                Console.WriteLine("connection successful");
+
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine("connection NOT successful");
+                Console.WriteLine(E.Message);
+            }
+        }
+
+
+    }
+}
