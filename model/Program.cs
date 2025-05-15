086010bcba8904dc4b8794dec033719dbb8f2408
Niket <ac150381@avcol.school.nz>
2025-05-16 09:58:00 +1200

    trying to connect to my database

--- model/Program.cs
+++ model/Program.cs
@@ -2,10 +2,16 @@
 {
     internal class Program
     {
+        private static Storagemanager storagemanager;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial" +
+                " Catalog=\"sports managment \";Integrated Security=True;Connect " +
+                "Timeout=30;Encrypt=False;Trust Server Certificate=False;Application " +
+                "Intent=ReadWrite;Multi Subnet Failover=False";
+            Storagemanager hello = new Storagemanager(connectionString);
+            
         }
     }
 }
