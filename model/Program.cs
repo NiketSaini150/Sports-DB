82553fa0a82057109b4c6c66228ba75174708af8
Niket <ac150381@avcol.school.nz>
2025-05-27 18:02:17 +1200

    had to do the connection again while accessing it from my home laptop

--- model/Program.cs
+++ model/Program.cs
@@ -9,7 +9,7 @@ namespace Sports_DB.model
         static void Main(string[] args)
          {
 
-            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\ac150381.AC\\OneDrive - Avondale College\\Sports DB\\SportsPLSWORK.mdf\";Integrated Security=True;Connect Timeout=30";
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\niket\\OneDrive - Avondale College\\Sports DB\\SportsPLSWORK.mdf\";Integrated Security=True;Connect Timeout=30";
             storagemanager  = new Storagemanager(connectionString);
 
             storagemanager = new Storagemanager(connectionString);
